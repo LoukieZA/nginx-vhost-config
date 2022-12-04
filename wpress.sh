86e58140e9541c2c16e473dc7b0d7707998bce6e
Logan <logan.africa@lgatechnologies.co.za>
2022-12-05 00:50:11 +0200

    updated script

--- wpress.sh
+++ wpress.sh
@@ -4,40 +4,43 @@
 apt-get update && apt-get upgrade -y
 
 ##Install nginx
-apt-get install nginx 
+sudo apt-get install nginx -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
 ##Install MariaDB
 apt-get install mariadb-server -y
 systemctl enable mariadb.service
 mysql_secure_installation
 
-##install php
-apt-get install php8.1 php8.1-cli php8.1-fpm php8.1-mysql php8.1-json php8.1-opcache php8.1-mbstring php8.1-xml php8.1-gd php8.1-curl 
+##Install MariaDB and PHP
+sudo apt-get install php php-mysql php-fpm php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip mariadb-server mariadb-client php-json
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo systemctl start php8.1-fpm
+sudo systemctl enable php8.1-fpm
+sudo systemctl status php8.1-fpm
+sudo mysql_secure_installation
 
-#Download worpress and extract copy
+##Download worpress and extract copy
 wget https://wordpress.org/latest.tar.gz
 echo "The latest WordPress release has been downloaded.What do you want the name of your website to be?"
 read websitename
 tar -xzvf latest.tar.gz
 mkdir /var/www/$websitename
-cp -R ./wordpress/* /var/www/$websitename/ 
+cp -R ./wordpress/* /var/www/html/$websitename/
+sudo chown -R www-data:www-data /var/www/html/$websitename/
+sudo chmod -R 755 /var/www/html/$websitename/ 
 echo $websitename "has been extracted" 
-echo "Would you like me to configure your nginx code for" $websitename
-read nginxconfig
-if $nginxconfig="yes";
-then 
-   mv ./nginx-vhost-config/newhost.conf /etc/nginx/conf.d/$websitename.conf
-   sed -i "s/wordpress/$websitename/" /etc/nginx/conf.d/$websitename.conf
-   cd /etc/nginx/conf.d
-   touch $websitename.conf
-   
-else
-   echo "Nginx, php8.1, Mariadb has been installed. The latest WP release has been downloaded and extracted.Dont forget to configure nginx and your DB :)"
-fi
+echo "configuring the nginx webserver" $websitename
 
-echo "I will now configure your database"
-echo "your mysql username will be $websitename. Please specify the password for this user"
-read dbpassword
+##Replace git files defaults with defined variables 
+mv ./nginx-vhost-config/newhost.conf /etc/nginx/conf.d/$websitename.conf
+sed -i "s/wordpress/$websitename/" /etc/nginx/conf.d/$websitename.conf
+   
+   
+##DB creation and configuration
+echo "Nginx, php8.1, Mariadb has been installed. The latest WP release has been downloaded and extracted.Configuring DB :)"
 sed -i "s/wpress/$websitename" ./nginx-vhost-config/dbconfig.sql
 sed -i "s/wpressuser/$websitename" ./nginx-vhost-config/dbconfig.sql
 mysql -u root -p -e ./nginx-vhost-config/dbconfig.sql
